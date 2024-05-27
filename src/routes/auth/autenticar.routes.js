839f495f6db5878f3d2c3a5e6ccc24a7af58a551
Kutik-Gomez <kutikgomez@gmail.com>
2024-05-27 09:59:47 -0500

    se elimino para hacer de nuevo

--- src/routes/auth/autenticar.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { registrar, login } from '../../controllers/auth/autenticarController.js';
-
-const router = express.Router();
-
-router.post('/registrar', registrar);
-router.post('/login', login);
-
-export default router;
